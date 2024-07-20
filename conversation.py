c423198036a3494680b1685668e5d7f82ff42daf
B0sh <b0shbbl@hotmail.com>
2024-07-20 13:55:29 -0500

    most simpliest Chat gpt 対話

---
+++ conversation.py
@@ -0,0 +1,31 @@
+import os
+from openai import OpenAI
+
+class ConversationEngine:
+    def __init__(self):
+        self.client = OpenAI(
+            api_key = os.environ.get("OPENAI_API_KEY"),
+        )
+        self.messages = [
+            {
+                "role": "system",
+                "content": "日本語だけを使ってください。あなたは日本語教師です。日本語学習者と練習会話を開始します。"
+            }
+        ]
+
+
+    def send(self, input):
+        self.messages.append({"role": "user", "content": input})
+
+        response = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            max_tokens=256,
+            messages=self.messages
+        )
+
+        response = response.choices[0].message.content.strip()
+
+        self.messages.append({"role": "assistant", "content": response})
+
+        return response
+    
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import eel
 from audio import AudioTranscriptionEngine
+from conversation import ConversationEngine
 
 eel.init('web')
 
 audio = AudioTranscriptionEngine()
+conversation = ConversationEngine()
 
 @eel.expose
 def start_listening():
     print("Listening...")
     text = audio.listen()
+
+    if text:
+        conversation.send(text)
+
     return text
 
 eel.start('index.html', size=(800, 600))
